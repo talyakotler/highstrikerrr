78505cb005736e83f25b770ae27687e70613b92d
talyakotler <50055296+talyakotler@users.noreply.github.com>
2020-02-22 16:22:56 +0200

    Add files via upload

---
+++ HighStriker_html/jScripts/myScript.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".about").click(function () {
+        $("#aboutDiv").toggle();
+        $('#toggleBg').toggle();
+    });
+
+    $(".howToPlay").click(function () {
+        $("#howToPlayDiv").toggle();
+        $('#toggleBg').toggle();
+    });
+
+    $(".closeAbout").click(function () {
+        $('#toggleBg').hide();
+        $("#aboutDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    $(".closeHowToPlay").click(function () {
+        $('#toggleBg').hide();
+        $("#howToPlayDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+});
